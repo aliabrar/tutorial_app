ab028b985ba724ac6718c85378d91a4f30859e72
ali abrar <ali@obsidian-online.com>
2011-11-24 12:23:43 -0500

    created error messages for new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,16 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # TODO successful save
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
